b75abe58a2f365d170b16dcaa70b462067b90ef7
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-17 08:04:00 +0100

    added bar to main screen

--- App.tsx
+++ App.tsx
@@ -1,42 +1,44 @@
 import React from 'react';
 
 import {View, StyleSheet} from 'react-native';
+import {Bar} from './src/components/bottombar/Bar';
 
 import Pad from './src/components/pad/';
 
 const App = () => {
-  return (
-    <View style={styles.MainContainer}>
-      <Pad
-        strokes={[]}
-        containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
-        rewind={(undo: any) => {}}
-        clear={(clear: any) => {}}
-        color={'#000000'}
-        strokeWidth={4}
-        onChangeStrokes={(strokes: any) => console.log(strokes)}
-      />
-    </View>
-  );
+    return (
+        <View style={styles.MainContainer}>
+            <Pad
+                strokes={[]}
+                containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+                rewind={(undo: any) => {}}
+                clear={(clear: any) => {}}
+                color={'#000000'}
+                strokeWidth={4}
+                onChangeStrokes={(strokes: any) => console.log(strokes)}
+            />
+            <Bar />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  MainContainer: {
-    flex: 1,
-  },
+    MainContainer: {
+        flex: 1,
+    },
 
-  childView: {
-    flex: 1,
-    overflow: 'hidden',
-  },
-  point: {
-    height: 22,
-    width: 22,
-    marginTop: 5,
-    position: 'absolute',
-    borderRadius: 14,
-    backgroundColor: '#afeeee',
-  },
+    childView: {
+        flex: 1,
+        overflow: 'hidden',
+    },
+    point: {
+        height: 22,
+        width: 22,
+        marginTop: 5,
+        position: 'absolute',
+        borderRadius: 14,
+        backgroundColor: '#afeeee',
+    },
 });
 
 export default App;
