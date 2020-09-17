9237c09afcfbe37b3d823caf945caf510ac2cd84
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-17 07:56:31 +0100

    added BottomBar and ColorSelector

---
+++ src/components/bottombar/Bar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {StyleSheet, View} from 'react-native';
+import {ColorSelector} from './tools/ColorSelector';
+
+export const Bar = () => {
+  return (
+    <View style={styles.containerStyle}>
+      <ColorSelector />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    backgroundColor: '#fff',
+  },
+});
---
+++ src/components/bottombar/tools/ColorSelector.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {StyleSheet, View} from 'react-native';
+
+export const ColorSelector = () => {
+  return (
+    <View>
+      <View style={styles.circleButtonStyle} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  circleButtonStyle: {
+    height: 22,
+    width: 22,
+    marginTop: 5,
+    borderRadius: 50,
+    backgroundColor: 'red',
+  },
+});
