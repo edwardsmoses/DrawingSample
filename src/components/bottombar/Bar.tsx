0b662401172a554910b353eef73af7b8b0fc4c89
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-17 15:10:59 +0100

    added undoButton

--- src/components/bottombar/Bar.tsx
+++ src/components/bottombar/Bar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {StyleSheet, View} from 'react-native';
 import {ColorSelector} from './tools/ColorSelector';
+import {UndoButton} from './tools/UndoButton';
 
 type BarProps = {
     selectColor(color: string): void;
@@ -9,11 +10,16 @@ type BarProps = {
 };
 
 export const Bar = (props: BarProps) => {
-    const {selectColor} = props;
+    const {selectColor, undoAction} = props;
 
     return (
         <View style={styles.containerStyle}>
-            <ColorSelector onSelectColor={selectColor} />
+            <View style={styles.topSection}>
+                <UndoButton undoAction={undoAction} />
+            </View>
+            <View style={styles.bottomSection}>
+                <ColorSelector onSelectColor={selectColor} />
+            </View>
         </View>
     );
 };
@@ -22,10 +28,17 @@ const styles = StyleSheet.create({
     containerStyle: {
         backgroundColor: '#fff',
         width: '100%',
-        height: 70,
+        height: 100,
         justifyContent: 'center',
         alignItems: 'center',
         position: 'absolute',
         bottom: 0,
+    },
+    topSection: {
+        flexDirection: 'row',
+        flex: 1,
+    },
+    bottomSection: {
+        flexDirection: 'row',
     },
 });
