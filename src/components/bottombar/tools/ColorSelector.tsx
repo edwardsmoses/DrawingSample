253a3b23cf8ef918603c5655457171e8887ae53c
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-17 08:17:09 +0100

    added Color Button Selector

--- src/components/bottombar/tools/ColorSelector.tsx
+++ src/components/bottombar/tools/ColorSelector.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 
-export const ColorSelector = () => {
+type ColorButtonProps = {
+    onPress(): void;
+    buttonColor: string;
+};
+
+const ColorButton = (props: ColorButtonProps) => {
+    const {onPress, buttonColor} = props;
+
+    const customStyles = {
+        backgroundColor: buttonColor,
+        borderColor: buttonColor,
+        borderWidth: 1,
+    };
+    return (
+        <TouchableOpacity onPress={() => onPress()}>
+            <View style={[styles.circleButtonStyle, customStyles]} />
+        </TouchableOpacity>
+    );
+};
+
+type ColorSelectorProps = {
+    onSelectColor(): void;
+};
+
+export const ColorSelector = (props: ColorSelectorProps) => {
+    const {onSelectColor} = props;
+    const colors = ['red', 'green'];
+
     return (
         <View style={styles.containerStyle}>
-            <View style={styles.circleButtonStyle} />
+            {colors.map((color, index) => (
+                <ColorButton
+                    onPress={onSelectColor}
+                    buttonColor={color}
+                    key={`${color}-${index}`}
+                />
+            ))}
         </View>
     );
 };
@@ -13,12 +46,13 @@ export const ColorSelector = () => {
 const styles = StyleSheet.create({
     containerStyle: {
         alignSelf: 'flex-end',
+        flex: 1,
+        flexDirection: 'row',
     },
     circleButtonStyle: {
         height: 22,
         width: 22,
         marginTop: 5,
         borderRadius: 50,
-        backgroundColor: 'red',
     },
 });
