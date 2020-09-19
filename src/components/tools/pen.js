2b0291a0664fb98b9491c997ca315ca1d9fc610c
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-19 20:58:17 +0100

    updated the tooling to
    'https://github.com/jayeszee/rn-draw/tree/0dec6a5a0304133f2af728808151c672d780750b'

--- src/components/tools/pen.js
+++ src/components/tools/pen.js
@@ -1,50 +1,59 @@
+import simplify from "./simplify";
+
+const d3 = require("d3-shape")
+const defaultLineGenerator = d3.line()
+  .x(function (d) { return d.x; })
+  .y(function (d) { return d.y; })
+  .curve(d3.curveCatmullRom.alpha(0.5));
+
 export default class Pen {
-    constructor(strokes) {
-        this.strokes = strokes || [];
-        this._offsetX = 0;
-        this._offsetY = 0;
-    }
+  constructor(strokes) {
+    this.strokes = strokes || [];
+    this._offsetX = 0;
+    this._offsetY = 0;
+  }
 
-    addStroke(points) {
-        if (points.length > 0) {
-            this.strokes.push(points);
-        }
+  addStroke(points) {
+    if (points.length > 0) {
+      this.strokes.push(points);
     }
+  }
 
-    rewindStroke() {
-        if (this.strokes.length < 1) {
-            return;
-        }
-        this.strokes.pop();
+  rewindStroke() {
+    if (this.strokes.length < 1) return
+    this.strokes.pop()
+  }
+
+  setOffset(options) {
+    if (!options) return
+    this._offsetX = options.x;
+    this._offsetY = options.y;
+  }
+
+  pointsToSvg(points, tolerance = 1, lineGenerator = null, highQuality = true) {
+    let offsetX = this._offsetX;
+    let offsetY = this._offsetY;
+    if (points.length > 0) {
+      let simplifiedPathPoints = simplify(points, tolerance, highQuality);
+      /*       var path = `M ${points[0].x},${points[0].y}`
+            simplified.forEach((point) => {
+              path = path + ` L ${point.x},${point.y}`
+            }); */
+      // var path = `M ${points[0].x},${points[0].y}`
+      if (lineGenerator && typeof lineGenerator === "function") {
+        return lineGenerator(simplifiedPathPoints);
+      }
+      return defaultLineGenerator(simplifiedPathPoints);
+    } else {
+      return ''
     }
+  }
 
-    setOffset(options) {
-        if (!options) {
-            return;
-        }
-        this._offsetX = options.x;
-        this._offsetY = options.y;
-    }
+  clear = () => {
+    this.strokes = []
+  }
 
-    pointsToSvg(points) {
-        let offsetX = this._offsetX;
-        let offsetY = this._offsetY;
-        if (points.length > 0) {
-            var path = `M ${points[0].x},${points[0].y}`;
-            points.forEach((point) => {
-                path = path + ` L ${point.x},${point.y}`;
-            });
-            return path;
-        } else {
-            return '';
-        }
-    }
-
-    clear = () => {
-        this.strokes = [];
-    };
-
-    copy() {
-        return new Reaction(this.strokes.slice());
-    }
+  copy() {
+    return new Reaction(this.strokes.slice());
+  }
 }
