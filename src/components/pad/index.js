12db6cc4fe19dd4464995ba95b6eabcdcfbd9398
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-19 13:58:10 +0100

    lint

--- src/components/pad/index.js
+++ src/components/pad/index.js
@@ -6,7 +6,6 @@ import Point from '../tools/point';
 
 import humps from 'humps';
 
-
 export const convertStrokesToSvg = (strokes, layout = {}) => {
     return `
     <svg xmlns="http://www.w3.org/2000/svg" width="${layout.width}" height="${
