435574964007dace727344ab3494182163b83193
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-27 20:25:12 +0100

    replaced useMemo to useRef.
    Bcos of this:
    React.memo only checks for prop changes. If your function component wrapped in React.memo has a useState or useContext Hook in its implementation, it will still rerender when state or context change.
    
    And this:
    A ref created with useRef will be created only when the component has been mounted and preserved for the full lifecycle.

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -3,36 +3,34 @@ import React from 'react';
 import {View, PanResponder, StyleSheet} from 'react-native';
 
 export const Canvas = () => {
-    const panResponder = React.useMemo(
-        () =>
-            PanResponder.create({
-                onStartShouldSetPanResponder: (evt, gestureState) => true,
-                onMoveShouldSetPanResponder: (evt, gestureState) => true,
+    const panResponder = React.useRef(
+        PanResponder.create({
+            onStartShouldSetPanResponder: (evt, gestureState) => true,
+            onMoveShouldSetPanResponder: (evt, gestureState) => true,
 
-                onPanResponderGrant: (evt, gestureState) => {
-                    console.log(
-                        'Touched',
-                        evt.nativeEvent.locationX,
-                        evt.nativeEvent.locationY,
-                    );
-                },
-                onPanResponderMove: (evt, gestureState) => {
-                    console.log(
-                        'Moved',
-                        evt.nativeEvent.locationX,
-                        evt.nativeEvent.locationY,
-                    );
-                },
-                onPanResponderRelease: (evt, gestureState) => {
-                    console.log(
-                        'Released',
-                        evt.nativeEvent.locationX,
-                        evt.nativeEvent.locationY,
-                    );
-                },
-            }),
-        [],
-    );
+            onPanResponderGrant: (evt, gestureState) => {
+                console.log(
+                    'Touched',
+                    evt.nativeEvent.locationX,
+                    evt.nativeEvent.locationY,
+                );
+            },
+            onPanResponderMove: (evt, gestureState) => {
+                console.log(
+                    'Moved',
+                    evt.nativeEvent.locationX,
+                    evt.nativeEvent.locationY,
+                );
+            },
+            onPanResponderRelease: (evt, gestureState) => {
+                console.log(
+                    'Released',
+                    evt.nativeEvent.locationX,
+                    evt.nativeEvent.locationY,
+                );
+            },
+        }),
+    ).current;
 
     return (
         <View
