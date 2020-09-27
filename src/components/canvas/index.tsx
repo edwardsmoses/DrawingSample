5ab3af90d80df982a743e2119e0bb8ebe2d2b25b
Edwards Moses <edwardsmoses3@gmail.com>
2020-09-27 20:01:39 +0100

    would use useMemo React only calls the function when the value is needed. On top of that React also stores previous values given the inputs and will return the previous value given the same previous inputs.

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 import {View, PanResponder} from 'react-native';
 
 export const Canvas = () => {
-    const panResponder = PanResponder.create({
-        onStartShouldSetPanResponder: (evt, gestureState) => true,
-        onPanResponderMove: (evt, gestureState) => {},
-        onPanResponderRelease: (evt, gestureState) => {},
-    });
+    const panResponder = React.useMemo(
+        () =>
+            PanResponder.create({
+                onStartShouldSetPanResponder: (evt, gestureState) => true,
+                onPanResponderMove: (evt, gestureState) => {},
+                onPanResponderRelease: (evt, gestureState) => {},
+            }),
+        [],
+    );
 
     return <View />;
 };
